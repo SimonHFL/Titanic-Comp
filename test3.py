a541c8c0626ad1e656ef28c972de234c823ec22d
Simon Flachs <simon@hivelocity.co.jp>
2015-04-28 17:17:11 +0900

    refactored

--- test3.py
+++ test3.py
@@ -4,38 +4,50 @@ import pylab as P
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier 
 
+
+def setGender(df):
+	df['Gender'] = df['Sex'].map( {'female': 0, 'male': 1}).astype(int)
+	return df
+
+def setEmbarked(df):
+	df['Embarked'] = df['Embarked'].map( { 'C':0, 'S':1, 'Q':2} )
+	# temporary fix
+	df['Embarked'][df['Embarked'].isnull()] = 0
+
+	return df
+
+def setAgeFill(df):
+
+	median_ages = np.zeros((2,3))
+
+	for i in range(0,2):
+		for j in range(0,3):
+			median_ages[i,j] = df[(df['Gender'] == i) & (df['Pclass'] == j+1)]['Age'].dropna().median()
+
+	df['AgeFill'] = df['Age']		
+
+	for i in range(0,2):
+		for j in range(0,3):
+			df.loc[ (df.Age.isnull()) & (df.Gender == i) & (df.Pclass == j+1), 'AgeFill'] = median_ages[i,j]
+
+	return df
+
+def dropColumns(df):
+	df = df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId', 'Age'], axis = 1)
+	return df
+
+
+
+
 df = pd.read_csv('train.csv', header=0)
 
+df = setGender(df)
 
-# set gender to 0/1 value
-df['Gender'] = df['Sex'].map( {'female': 0, 'male': 1}).astype(int)
+df = setEmbarked(df)
 
-# set missing ages
-median_ages = np.zeros((2,3))
+df = setAgeFill(df)
 
-for i in range(0,2):
-	for j in range(0,3):
-		median_ages[i,j] = df[(df['Gender'] == i) & (df['Pclass'] == j+1)]['Age'].dropna().median()
-
-
-print(median_ages)
-
-df['AgeFill'] = df['Age']		
-
-for i in range(0,2):
-	for j in range(0,3):
-		df.loc[ (df.Age.isnull()) & (df.Gender == i) & (df.Pclass == j+1), 'AgeFill'] = median_ages[i,j]
-
-# set embarked port to 0/1/2
-
-df['Embarked'] = df['Embarked'].map( { 'C':0, 'S':1, 'Q':2} )
-# temporary fix
-df['Embarked'][df['Embarked'].isnull()] = 0
-
-#print(df.dtypes[df.dtypes.map(lambda x: x=='object')])
-
-#remove columns
-df = df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId', 'Age'], axis = 1)
+df = dropColumns(df)
 
 train_data = df.values
 
@@ -45,36 +57,18 @@ train_data = df.values
 
 
 
-
-
-
 test_df = pd.read_csv('test.csv', header=0)
 
-# set gender to 0/1 value
-test_df['Gender'] = test_df['Sex'].map( {'female': 0, 'male': 1}).astype(int)
+test_df = setGender(test_df)
 
-# set missing ages
-median_ages = np.zeros((2,3))
+test_df = setAgeFill(test_df)
 
-for i in range(0,2):
-	for j in range(0,3):
-		median_ages[i,j] = test_df[(test_df['Gender'] == i) & (test_df['Pclass'] == j+1)]['Age'].dropna().median()
+test_df = setEmbarked(test_df)
 
 
-print(median_ages)
-
-test_df['AgeFill'] = test_df['Age']		
-
-for i in range(0,2):
-	for j in range(0,3):
-		test_df.loc[ (test_df.Age.isnull()) & (test_df.Gender == i) & (test_df.Pclass == j+1), 'AgeFill'] = median_ages[i,j]
-
-
-#set embarked port to 0/1/2
-
-test_df['Embarked'] = test_df['Embarked'].map( { 'C':0, 'S':1, 'Q':2} )
+#test_df['Embarked'] = test_df['Embarked'].map( { 'C':0, 'S':1, 'Q':2} )
 # temporary fix
-test_df['Embarked'][test_df['Embarked'].isnull()] = 0
+#test_df['Embarked'][test_df['Embarked'].isnull()] = 0
 
 # correct fare
 median_fare = np.zeros(3)
@@ -88,7 +82,7 @@ for f in range(0,3):                                              # loop 0 to 2
 ids = test_df['PassengerId'].values
 
 
-test_df = test_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId', 'Age'], axis = 1)
+test_df = dropColumns(test_df)
 
 test_data = test_df.values
 
